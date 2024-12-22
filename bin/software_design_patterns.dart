37f16f931e574970224d4cc05d094085899cef41
HAROONABUTABAR <haroonabutabar9966@gmail.com>
2024-12-22 08:08:51 +0300

    single responsibility review and example

--- bin/software_design_patterns.dart
+++ bin/software_design_patterns.dart
@@ -3,3 +3,5 @@ void main(List<String> arguments) {}
 /// Interface Software Design Patterns
 // Best Practice to solve common software problems
 // solutions in the form of templates that may be applied to real-world problems
+
+
--- lib/single_responsibility_principle.dart
+++ lib/single_responsibility_principle.dart
@@ -6,4 +6,21 @@
 // Uncle Bob , "There should never be more than one reason for a class to change"
 // A class should have one and only one reason to change , meaning that a class should have only one job
 
+// Example
+class PaymentProcessor {
+  void charge(double amount) {
+    // initialize bank data
+    // send request to the bank
+  }
+  String createReport() {
+    // format a report
+    return "report";
+  }
 
+  void printReport() {
+    // sned a printing command
+  }
+  void savePayment() {
+    // saving to DB
+  }
+}
