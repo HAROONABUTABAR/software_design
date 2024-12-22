c4a33936ac3b146c4cf6a19529c3826b900ae400
HAROONABUTABAR <haroonabutabar9966@gmail.com>
2024-12-22 08:04:28 +0300

    first commit

---
+++ lib/single_responsibility_principle.dart
@@ -0,0 +1,9 @@
+//Programming Terminology
+// implamentation -> Code is written in a single file
+//---------------------------------------------------------
+// Single Responsibility Principle (S.R.P)
+// "Do one thing and do it well"
+// Uncle Bob , "There should never be more than one reason for a class to change"
+// A class should have one and only one reason to change , meaning that a class should have only one job
+
+
---
+++ lib/solid_principles.dart
@@ -0,0 +1,17 @@
+// Single Responsibility Principle (SRP)
+// A class should have only one reason to change
+
+
+//  Open Closed Principle (OCP)
+// The class should be open for extension but closed for modification
+
+
+// Liskov Substitution Principle (LSP)
+// If you substitute any type with one of it's subtypes, the behavior should not change
+
+// Interface Segregation Principle (ISP)
+// Avoid making general interface contains all methods.
+
+//Dependency Inversion Principle (DIP)
+// High-level classes should not know the implementation of low-level classes but depends on abstractions
+
